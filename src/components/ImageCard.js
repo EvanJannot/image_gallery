0bbfacec55f2e5b8087a248197f7dc86405ebfa8
JANNOT <evan.jannot@sogeti.com>
2023-01-02 18:40:44 +0100

    Implement search bar

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,8 +3,7 @@ import React from 'react'
 const ImageCard = ({image}) => {
     const tags = image.tags.split(',');
   return (
-    <div>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg"> 
+    <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto "> 
       <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
@@ -26,14 +25,13 @@ const ImageCard = ({image}) => {
         </ul>
       </div>
       <div className="px-6 py-4">
-        {tags.map(tag => (
-            <span key={image.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        {tags.map((tag, index) => (
+            <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
             #{tag}
             </span>
         ))}
     </div>
     </div>
-  </div>
   )
 }
 
